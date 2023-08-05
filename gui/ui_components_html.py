502409252b5a7f631e533e70e6ccb9240ca904f7
Tarang Negandhi <protikuda@gmail.com>
2023-08-05 07:42:11 +0530

    UI-CHANGED THEME

--- gui/ui_components_html.py
+++ gui/ui_components_html.py
@@ -4,16 +4,9 @@ class GradioComponentsHTML:
     def get_html_header() -> str:
         '''Create HTML for the header'''
         return '''
-            <div style="display: flex; justify-content: space-between; align-items: center; padding: 5px;">
-            <h1 style="margin-left: 0px; font-size: 35px;">ShortGPT</h1>
-            <div style="flex-grow: 1; text-align: right;">
-                <a href="https://discord.gg/uERx39ru3R" target="_blank" style="text-decoration: none;">
-                <button style="margin-right: 10px; padding: 10px 20px; font-size: 16px; color: #fff; background-color: #7289DA; border: none; border-radius: 5px; cursor: pointer;">Join Discord</button>
-                </a>
-                <a href="https://github.com/RayVentura/ShortGPT" target="_blank" style="text-decoration: none;">
-                <button style="padding: 10px 20px; font-size: 16px; color: #fff; background-color: #333; border: none; border-radius: 5px; cursor: pointer;">Add a Star on Github ⭐</button>
-                </a>
-            </div>
+            <div style="display: flex; justify-content: space-between; align-items: center; padding: 1em; background-color: #004165;">
+                <img src="https://www.anz.com.au/content/dam/anzconz/images/common/promopages/logo-promo-anz-small.png">
+                <h1 style="margin-left: 0px; font-size: 35px;">GyanzAI Video and Audio</h1>
             </div>
         '''
 
